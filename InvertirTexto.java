50d4736580caa43d8a8a957ad7c7ca18c2b9138f
jkmilo24 <jcamilomc21@gmail.com>
2025-02-22 20:20:58 -0500

    Update InvertirTexto.java

--- InvertirTexto.java
+++ InvertirTexto.java
@@ -4,15 +4,11 @@ public class InvertirTexto {
     public static void main(String[] args) {
         Scanner jk = new Scanner (System.in);
 
-        // aca se solicita al usuario ingresar el texto de cadena
-
         System.out.print("ingresar el texto a invertir:");
         String texto = jk.nextLine();
 
-        // variable para almacenar el texto a invertir
         String textoInvertido = "";
 
-        // Recorrer la cadena desde el final hasta el principio y agregar cada letra a la nueva cadena
         for (int i = texto.length() - 1; i >= 0; i--) {
             textoInvertido += texto.charAt(i);
         }
